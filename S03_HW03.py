7d9725e9a8df62ec97e296e7a0ded32e8a4bd960
StanleyKeys <108968816+StanleyKeys@users.noreply.github.com>
2022-09-11 22:08:24 +0700

    Seminar03_HomeWork03
    Задайте список из вещественных чисел. 
    Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.

---
+++ S03_HW03.py
@@ -0,0 +1,31 @@
+"""
+Задайте список из вещественных чисел. 
+Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+"""
+def UserInput(inputText):                                   # Метод ввода текста
+    userEnter = input(f"{inputText}")
+    return userEnter                                  
+
+
+def FillList(list):                                         # Метод заполнения списка
+    userList = []                                                
+    for i in list:
+        userList.append(float(i))   
+    return userList
+
+userEnter = UserInput("Введите вещественные числа через пробел: ")
+splitList = userEnter.split(" ")
+newList = []
+newList = FillList(splitList)
+
+integerList = []
+for i in newList:
+    integerNumber = i - int(i)
+    integerList.append(integerNumber)
+    
+differ = max(integerList) - min(integerList)                # Функция расчет максимального и минимального числа в списке  
+
+answer = float(int(differ*100)/100)                          #Перевод вещественного числа в целое с умножением 2-ух остатков и обратно для красоты
+
+print(f"Разница между максимальным и минимальным значением дробной части элементов: {answer}")
+
