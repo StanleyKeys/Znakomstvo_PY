0519a310973d313d15031324c8fe13ac90513923
StanleyKeys <108968816+StanleyKeys@users.noreply.github.com>
2022-09-12 12:45:29 +0700

    Seminar04_HW02
    Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.

---
+++ S04_HW02.py
@@ -0,0 +1,21 @@
+"""
+Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+"""
+
+userEnter = int(input("Введите натуральное число N: "))
+
+def Factor(y):                                      # Факторизация целых чисел
+    list = []
+    x = 2
+    while x * x <= y:
+        if y % x == 0:
+            list.append(x)
+            y //= x
+        else:
+            x += 1
+    if y > 1:
+        list.append(y)
+    return list
+
+print(f"Список простых множителей числа {userEnter} равен: ")
+print(Factor(userEnter))
