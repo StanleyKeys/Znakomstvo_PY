60b7fab7a6d3c17b7e7a583014cf588a16f00882
StanleyKeys <108968816+StanleyKeys@users.noreply.github.com>
2022-09-13 12:05:38 +0700

    Update S04_HW04.py
    Косметические исправления: код для очистки терминала и зациклил программу с решением пользователя

--- S04_HW04.py
+++ S04_HW04.py
@@ -5,42 +5,55 @@
 
 - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 """
-import random
-indexes = {"0": "\u2070",
-           "1": "\u00B9",
-           "2": "\u00B2",
-           "3": "\u00B3",
-           "4": "\u2074",
-           "5": "\u2075",
-           "6": "\u2076",
-           "7": "\u2077",
-           "8": "\u2078",
-           "9": "\u2079",
-           "-": "\u207B"
-           }
+import os                                   # Код
+clear = lambda: os.system('cls')            # очистки
+clear()                                     # терминала
 
-k = input("Введите натуральную степень k: ")
-l = input("Введите натуральную степень l, если она есть: ")
+def ProgrammCycle():
+    indexes = {"0": "\u2070",
+            "1": "\u00B9",
+            "2": "\u00B2",
+            "3": "\u00B3",
+            "4": "\u2074",
+            "5": "\u2075",
+            "6": "\u2076",
+            "7": "\u2077",
+            "8": "\u2078",
+            "9": "\u2079",
+            "-": "\u207B"
+            }
 
-def DegreeCheck(degree):
-    if (degree != ""):                                       # Проверка полученного числа k, l на положительную степень
-        degree = int(degree)
-        if (degree < 2):
-            degree = ""
-    return degree
+    k = input("Введите натуральную степень k: ")
+    l = input("Введите натуральную степень l, если она есть: ")
 
-
-a = 0
-def RandomNumbers(d):                               # Заполение уравнения случайными числами
-    d = random.randint(2, 100)
-    return d
-
-def DegreePrint(a: int):                                 # Метод написания степени у многочлена
-    degrees = ""
-    temp = str(a)
-    for char in temp:
-        degrees += indexes[char] or ""
-    return degrees
+    def DegreeCheck(degree):
+        if (degree != ""):                                       # Проверка полученного числа k, l на положительную степень
+            degree = int(degree)
+            if (degree < 2):
+                degree = ""
+        return degree
 
 
-print(f"Уравнение: {RandomNumbers(a)}x{DegreePrint(DegreeCheck(k))} + {RandomNumbers(a)}y{DegreePrint(DegreeCheck(l))} - {RandomNumbers(a)} = 0")
+    a = 0
+    def RandomNumbers(d):                               # Заполение уравнения случайными числами
+        d = random.randint(2, 100)
+        return d
+
+    def DegreePrint(a: int):                                 # Метод написания степени у многочлена
+        degrees = ""
+        temp = str(a)
+        for char in temp:
+            degrees += indexes[char] or ""
+        return degrees
+
+
+    print(f"Уравнение: {RandomNumbers(a)}x{DegreePrint(DegreeCheck(k))} + {RandomNumbers(a)}y{DegreePrint(DegreeCheck(l))} - {RandomNumbers(a)} = 0")
+
+ProgrammCycle()
+while True:
+    flag = input('\nЕщё раз? [yes/no]: ')
+    if (flag == 'y') or (flag == 'Yes') or (flag == 'Y'):
+        ProgrammCycle()
+    if (flag == 'n') or (flag == 'no') or (flag == 'N'):
+        break
+
